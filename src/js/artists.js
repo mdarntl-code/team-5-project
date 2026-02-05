8c9704380f0f47d2ab808d86bde0579ce59463a9
Daryna Yusikova <darynayusikova@gmail.com>
2026-02-05 17:29:12 +0000

    Added render function for artists section---- finished , started wroking on styles and functionality ----in process

--- src/js/artists.js
+++ src/js/artists.js
@@ -1,1 +1,19 @@
 console.log('artists');
+
+import { getArtists } from './api';
+import { renderArtistCards } from './render-functions';
+
+const page = 1;
+const limit = 8;
+
+// Розкоментувати щоб перевірити завантаження карток
+// async function loadArtists() {
+//   try {
+//     const { data } = await getArtists({ page, limit });
+//     renderArtistCards(data.artists);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// loadArtists();
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,3 +1,42 @@
-// "Колеги, файл render-functions.js — це наш конструктор HTML. 
-// Давайте домовимось: вся генерація HTML-рядків живе тільки там. У файлах логіки (api, main) ми просто викликаємо ці функції. 
+// "Колеги, файл render-functions.js — це наш конструктор HTML.
+// Давайте домовимось: вся генерація HTML-рядків живе тільки там. У файлах логіки (api, main) ми просто викликаємо ці функції.
 // Так ми не заплутаємось у коді і зможемо легко правити верстку, не ламаючи логіку JS."
+
+const artistsList = document.querySelector('.artists-list');
+
+//Функція рендеру карток артистів, потрібно передати масив списку артистів.
+export function renderArtistCards(artists) {
+  const markup = artists
+    .map(({ _id, strArtist, strArtistThumb, genres, strBiographyEN }) => {
+      const genresMarkup = genres
+        .map(genre => `<span class="artist-genre">${genre}</span>`)
+        .join('');
+
+      return `
+          <li class="artist-card">
+            <img
+              src="${strArtistThumb}"
+              alt="${strArtist}"
+              class="artist-img"
+            />
+            <div class="artist-genres">
+              ${genresMarkup}
+            </div>
+            <h4 class="artist-name">${strArtist}</h4>
+            <p class="artist-description">
+              ${strBiographyEN}
+            </p>
+            <button
+              type="button"
+              class="artist-link"
+              data-id="${_id}"
+              >
+              Learn more
+            </button>
+          </li>
+        `;
+    })
+    .join('');
+
+  artistsList.insertAdjacentHTML('beforeend', markup);
+}
